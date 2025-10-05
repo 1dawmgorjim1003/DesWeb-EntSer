bf50d07a3cbe26f3de61e2fe7d1ab6d41f50a3a3
Miguel <mgorjim1003@g.educaand.es>
2025-10-05 13:33:19 +0200

    Solución al ejercicio 3 [05/10/2025 13:33]

--- index.php
+++ index.php
@@ -1,103 +1,23 @@
 <?php
 
-/*INICIALIZACIÓN DE ENTORNO */
+//INICIALIZACIÓN DEL ENTORNO
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-/*Zona de declaración de funciones */
-//FUNCIÓN DE DEBUGUEO
-function dump($var){
-    echo '<pre>'.print_r($var,1).'</pre>';
-}
+include('lib/functions.php');
 
-//LÓGICA DE NEGOCIO
-function cargarTablero($rutaCSV) {
-    $tablero = [];
-    //Se comprueba si el archivo existe y tiene permisos de lectura
-    if (!is_readable($rutaCSV)) {
-        echo 'No se puede leer el fichero CSV: ' . $rutaCSV;
-    } else {
-        //Se abre el fichero en modo lectura (fopen y r de read)
-        if (($puntero = fopen($rutaCSV, 'r')) !== false) {
-            //Se lee el fichero línea a línea. Cada línea es un array, se separa por comas
-            /*El separador por defecto es la coma, si lo quiero cambiar es fgetcsv($puntero, 0, ";"); El 0 indica que la longitud de la linea es ilimitada */ 
-            while (($fila = fgetcsv($puntero)) !== false) {
-                //Ignora líneas vacías, si esta vacía se pasa a la siguiente línea
-                if ($fila === null || $fila === [null]) { continue; }
-                //Vamos añadiendo la fila leída a $tablero
-                $tablero[] = $fila;
-            }
-            // Cerramos la lectura del fichero
-            fclose($puntero);
-        }
-        return $tablero;
-    }
-}
-
-$archivoCSV = __DIR__ . '/data/tablero.csv';
-$tablero = cargarTablero($archivoCSV);
-
-//Cogemos posiciones de URL en la parte cliente (http://localhost/dESWeb-EntServ/UT3/Acts/index.php?col=4&row=4)
-//Creamos una función para comprobar las posiciones del personaje
-function leerInput(){
-    
-    $col = filter_input(INPUT_GET, 'col', FILTER_VALIDATE_INT);
-    $row = filter_input(INPUT_GET, 'row', FILTER_VALIDATE_INT);
-
-    return ((isset($col) && is_int($col)) && (isset($row)) && is_int($row))? array(
-            'row' => $row,
-            'col' => $col
-        ) : null;    
-
-    // dump($row);
-}
-
-
-//LÓGICA DE PRESENTACIÓN
-function getTableroMarkup ($tablero, $posPersonaje){
-    $output = '';
-    foreach ($tablero as $filaIndex => $datosFila) {
-        foreach ($datosFila as $columnaIndex => $tileType) {
-            if(isset($posPersonaje)&&($filaIndex == $posPersonaje['row'])&&($columnaIndex == $posPersonaje['col'])){
-                $output .= '<div class = "tile ' . $tileType . '"><img src="src/character.png"></div>';    
-            }else{
-                $output .= '<div class = "tile ' . $tileType . '"></div>';
-            }
-        }
-    }
-    return $output;
-}
-
+//LOGICA DE NEGOCIO
 $posPersonaje = leerInput();
-$tableroMarkup = getTableroMArkup($tablero, $posPersonaje);
+$arrows = getArrows($posPersonaje);
+$tablero = cargarTablero('data/tablero.csv');
 
 
-
-//Creamos una función para pintar un mensaje de error o de éxito
-function displayError() {
-    $output;
-    //Si la función devuelve false indicamos que no ha sido posible pintar el personaje. Si devuelve true, confirmamos que se ha pintado
-    if (leerInput() == null) {
-        $output = '<p>El personaje no ha podido cargarse correctamente. Se han dado posiciones inválidas o no se han indicado.</p>';
-    } else {
-        $output = '<p>El personaje se ha podido cargar correctamente.</p>';
-    }
-    return $output;
-}
-
-//Lógica de negocio
-//El tablero es un array bidimensional en el que cada fila contiene 12 palabras cuyos valores pueden ser:
-// agua
-// fuego
-// tierra
-// hierba
-
-//Lógica de presentación
-
+//LOGICA DE PRESENTACIÓN
+$tableroMarkup = getTableroMarkup($tablero, $posPersonaje);
+$arrowsMarkup = getArrowsMarkup($arrows);
 
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -146,8 +66,11 @@ function displayError() {
 </head>
 <body>
     <h1>Tablero juego super rol DWES</h1>
+    <div style="display: flex;">
+        <?php echo $arrowsMarkup; ?>
+    </div>
     <div class="contenedorTablero">
-        <?php echo $tableroMarkup; ?>
+        <?php echo $tableroMarkup;?>
     </div>
     <?php echo displayError() ?>
 </body>
---
+++ lib/functions.php
@@ -0,0 +1,129 @@
+<?php
+
+/*INICIALIZACIÓN DE ENTORNO */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+/*Zona de declaración de funciones */
+//FUNCIÓN DE DEBUGUEO
+function dump($var){
+    echo '<pre>'.print_r($var,1).'</pre>';
+}
+
+//LÓGICA DE NEGOCIO
+function cargarTablero($rutaCSV) {
+    $tablero = [];
+    if (!is_readable($rutaCSV)) {
+        echo 'No se puede leer el fichero CSV: ' . $rutaCSV;
+    } else {
+        if (($puntero = fopen($rutaCSV, 'r')) !== false) {
+            while (($fila = fgetcsv($puntero)) !== false) {
+                if ($fila === null || $fila === [null]) { continue; }
+                $tablero[] = $fila;
+            }
+            fclose($puntero);
+        }
+        return $tablero;
+    }
+}
+
+$archivoCSV = __DIR__ . '../../data/tablero.csv';
+$tablero = cargarTablero($archivoCSV);
+
+function leerInput(){
+    
+    $col = filter_input(INPUT_GET, 'col', FILTER_VALIDATE_INT);
+    $row = filter_input(INPUT_GET, 'row', FILTER_VALIDATE_INT);
+
+    return ((isset($col) && is_numeric($col)) && (isset($row)) && is_numeric($row))? array(
+            'row' => $row,
+            'col' => $col
+        ) : null;    
+
+}
+
+$posPersonaje = leerInput();
+
+function getArrows($posPersonaje) {
+    if(isset($posPersonaje)) {
+        $arrows = array(
+            'arriba' => array(
+                'col' => $posPersonaje['col'],
+                'row'=> $posPersonaje['row'] - 1,
+            ),
+            'abajo' => array(
+                'col' => $posPersonaje['col'],
+                'row' => $posPersonaje['row'] + 1,
+            ),
+            'izquierda' => array(
+                'col'=> $posPersonaje['col'] - 1,
+                'row' => $posPersonaje['row'],
+            ),
+            'derecha' => array(
+                'col' => $posPersonaje['col'] + 1,
+                'row' => $posPersonaje['row'],
+            ),
+        );
+        return $arrows;
+    }
+    return null;
+}
+
+
+//LÓGICA DE PRESENTACIÓN
+function getTableroMarkup ($tablero, $posPersonaje){
+    $output = '';
+    foreach ($tablero as $filaIndex => $datosFila) {
+        foreach ($datosFila as $columnaIndex => $tileType) {
+            if(isset($posPersonaje)&&($filaIndex == $posPersonaje['row'])&&($columnaIndex == $posPersonaje['col'])){
+                if ($posPersonaje['row'] >= 0 && $posPersonaje['row'] < 12 && $posPersonaje['col'] >= 0 && $posPersonaje['col'] < 12) {
+                    $output .= '<div class = "tile ' . $tileType . '"><img src="src/character.png"></div>';    
+                }
+            }else{
+                $output .= '<div class = "tile ' . $tileType . '"></div>';
+            }
+        }
+    }
+    return $output;
+}
+
+$tableroMarkup = getTableroMArkup($tablero, $posPersonaje);
+
+function displayError() {
+    $output;
+    global $posPersonaje;
+    if (leerInput() == null) {
+        $output = '<p>El personaje no ha podido cargarse correctamente. No se han indicado posiciones.</p>';
+    } else {
+        if ($posPersonaje['row'] >= 0 && $posPersonaje['row'] < 12 && $posPersonaje['col'] >= 0 && $posPersonaje['col'] < 12) {
+            $output = '<p>El personaje se ha podido cargar correctamente.</p>';
+        } else {
+            $output = '<p>El personaje no ha podido cargarse correctamente. Se han dado posiciones inválidas.</p>';
+        }
+        
+    }
+    return $output;
+}
+
+function getArrowsMarkup($arrows) {
+    $output = '';
+    if (isset($arrows)) {
+        foreach ($arrows as $arrayI => $arrayJ) {
+            $output .= '<p><a href="index.php?col='.$arrayJ['col'].'&row='.$arrayJ['row'].'">'.$arrayI.'</a></p><p>// </p>';
+        }
+    }
+    return $output;
+}
+
+//Lógica de negocio
+//El tablero es un array bidimensional en el que cada fila contiene 12 palabras cuyos valores pueden ser:
+// agua
+// fuego
+// tierra
+// hierba
+
+//Lógica de presentación
+
+
+?>
